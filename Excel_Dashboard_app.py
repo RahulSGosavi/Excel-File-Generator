d653608b29dc404cee2c69a66292584516371062
Rahul Sanjay Gosavi <rahulgosavi624@gmail.com>
2025-02-12 16:25:17 +0530

    Update Excel_Dashboard_app.py

--- Excel_Dashboard_app.py
+++ Excel_Dashboard_app.py
@@ -1,26 +1,7 @@
 import streamlit as st
 import pandas as pd
-
-import subprocess
-import sys
-
-# Install matplotlib if it's missing
-try:
-    import matplotlib.pyplot as plt
-except ModuleNotFoundError:
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "matplotlib"])
-    import matplotlib.pyplot as plt
-
-print("Matplotlib is working!")
-
-# Install plotly if missing
-try:
-    import plotly.express as px
-except ModuleNotFoundError:
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "plotly"])
-    import plotly.express as px
-
-print("Plotly is working!")
+import matplotlib.pyplot as plt
+import plotly.express as px
 
 # File Uploader
 st.title(":bar_chart: Excel Data Visualization App")
